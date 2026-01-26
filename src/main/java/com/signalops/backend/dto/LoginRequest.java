d154baabbd5155f059a7a5479533b2b448a0a3ed
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:04:16 -0500

    Completed login request

--- src/main/java/com/signalops/backend/dto/LoginRequest.java
+++ src/main/java/com/signalops/backend/dto/LoginRequest.java
@@ -4,3 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+
+@Getter @Setter
+public class LoginRequest {
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
