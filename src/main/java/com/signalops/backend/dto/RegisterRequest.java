263bf948bce6e77a990f199cf053ca9cc0ee75c5
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 10:33:24 -0500

    Added getter and setter annoatitions with email and pass

--- src/main/java/com/signalops/backend/dto/RegisterRequest.java
+++ src/main/java/com/signalops/backend/dto/RegisterRequest.java
@@ -1,5 +1,15 @@
 package com.signalops.backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class RegisterRequest {
-	
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
