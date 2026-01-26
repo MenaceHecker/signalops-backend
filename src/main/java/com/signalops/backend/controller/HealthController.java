67c8b1281f6850da54129ced9eb3e9934ca33486
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:59:32 -0500

    Defined spring controller with single GET endpoint me

--- src/main/java/com/signalops/backend/controller/HealthController.java
+++ src/main/java/com/signalops/backend/controller/HealthController.java
@@ -1,5 +1,12 @@
 package com.signalops.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
-    
+    @GetMapping("/me")
+    public String me() {
+        return "You are authenticated";
+    }
 }
