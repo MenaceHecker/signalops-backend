9446f4ee0c9b550e2ac0d9759d5aeb4df2266f8e
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:32:21 -0500

    Fixed errors with user entity

--- src/main/java/com/signalops/backend/repository/UserRepository.java
+++ src/main/java/com/signalops/backend/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.signalops.backend.repository;
 
-public class UserRepository {
-    
+import com.signalops.backend.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
