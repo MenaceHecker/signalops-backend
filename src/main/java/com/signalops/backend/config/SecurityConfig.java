e3d0836c44d63b8f80565076932a942ecc4afb22
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:46:53 -0500

    Added all required imports

--- src/main/java/com/signalops/backend/config/SecurityConfig.java
+++ src/main/java/com/signalops/backend/config/SecurityConfig.java
@@ -1,5 +1,13 @@
 package com.signalops.backend.config;
 
-public class SecurityConfig {
-    
-}
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.*;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
