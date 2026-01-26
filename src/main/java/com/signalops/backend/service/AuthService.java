d7c7d5850d48408025ae705de05896a54e503f9e
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:53:19 -0500

    Added necessary imports

--- src/main/java/com/signalops/backend/service/AuthService.java
+++ src/main/java/com/signalops/backend/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.signalops.backend.service;
 
-public class AuthService {
-    
-}
+import com.signalops.backend.config.JwtUtil;
+import com.signalops.backend.dto.*;
+import com.signalops.backend.model.User;
+import com.signalops.backend.repository.UserRepository;
+import com.signalops.backend.util.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
