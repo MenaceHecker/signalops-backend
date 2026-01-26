bd95087700df8861938224a821f8b625ddf824e4
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:44:41 -0500

    Created filter class with override decorator

--- src/main/java/com/signalops/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/signalops/backend/config/JwtAuthenticationFilter.java
@@ -12,3 +12,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+}
