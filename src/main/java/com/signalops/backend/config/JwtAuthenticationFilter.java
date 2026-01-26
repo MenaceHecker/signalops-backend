782ca51aee8ac67444b33ec065a09b360cf95009
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 12:43:24 -0500

    Added al required imports

--- src/main/java/com/signalops/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/signalops/backend/config/JwtAuthenticationFilter.java
@@ -1,5 +1,14 @@
 package com.signalops.backend.config;
 
-public class JwtAuthenticationFilter {
-    
-}
+import com.signalops.backend.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
