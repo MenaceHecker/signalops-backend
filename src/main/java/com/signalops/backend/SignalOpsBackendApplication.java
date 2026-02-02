ad93272dc9108e470abac11dd5970a7f11c97a88
MenaceHecker <thedynamic2508@gmail.com>
2026-02-01 19:03:05 -0500

    Added primary source class

--- src/main/java/com/signalops/backend/SignalOpsBackendApplication.java
+++ src/main/java/com/signalops/backend/SignalOpsBackendApplication.java
@@ -3,6 +3,7 @@ package com.signalops.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main application class for SignalOps Backend
 @SpringBootApplication
 public class SignalopsBackendApplication {
     public static void main(String[] args) {
