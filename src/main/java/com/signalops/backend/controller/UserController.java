160325f58bf42ea69cadc1c724726692eac0875e
MenaceHecker <thedynamic2508@gmail.com>
2026-03-07 16:23:43 -0500

    Fixed the get method to fetch user profile

--- src/main/java/com/signalops/backend/controller/UserController.java
+++ src/main/java/com/signalops/backend/controller/UserController.java
@@ -3,10 +3,12 @@ package com.signalops.backend.controller;
 import com.signalops.backend.dto.UserProfileResponse;
 import com.signalops.backend.service.UserService;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -15,8 +17,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    // @GetMapping("/me")
-    // public UserProfileResponse me(Authentication auth) {
-    //     return userService.getMe(auth.getName());
-    // }
+    @GetMapping("/me")
+    public UserProfileResponse me(Authentication auth) {
+        return userService.getMe(auth.getName());
+    }
 }
