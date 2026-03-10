69f4097e04b16394b3a90f56d52ea76c2bb14028
MenaceHecker <thedynamic2508@gmail.com>
2026-03-10 10:13:27 -0400

    Declared and defined objects

--- src/main/java/com/signalops/backend/dto/CreateIncidentRequest.java
+++ src/main/java/com/signalops/backend/dto/CreateIncidentRequest.java
@@ -1,5 +1,31 @@
 package com.signalops.backend.dto;
 
 public class CreateIncidentRequest {
-    
+    private String title;
+    private String severity;
+    private String status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
