66ee96763000927f79b3a6cd851c849375cc3572
MenaceHecker <thedynamic2508@gmail.com>
2026-03-11 11:12:29 -0400

    Completed getStatus and setStatus func

--- src/main/java/com/signalops/backend/dto/UpdateIncidentStatusRequest.java
+++ src/main/java/com/signalops/backend/dto/UpdateIncidentStatusRequest.java
@@ -1,5 +1,13 @@
 package com.signalops.backend.dto;
 
 public class UpdateIncidentStatusRequest {
-    
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
